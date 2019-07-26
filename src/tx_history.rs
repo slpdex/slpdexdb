cd5466f405a8d9533b3532addcd32b069c6ecd1b
tobiasruck <ruck.tobias@gmail.com>
2019-07-26 22:52:25 +0200

    Refactored TxHistory, added some impls for SLPTxType, OutputType, TxType

--- src/tx_history.rs
+++ src/tx_history.rs
@@ -1,8 +1,8 @@
 use crate::tx_source::tx_result;
 use crate::config::SLPDEXConfig;
-use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use std::io;
-use cashcontracts::Output;
+use cashcontracts::{Output, AddressType};
 
 pub struct TxHistory {
     pub txs: Vec<HistoricTx>,
@@ -28,7 +28,7 @@ pub enum SLPTxType {
 pub enum TxType {
     Default,
     SLP {
-        token_id: [u8; 32],
+        token_hash: [u8; 32],
         version: i32,
         slp_type: SLPTxType,
     },
@@ -65,6 +65,56 @@ pub struct TradeOffer {
     pub receiving_address: cashcontracts::Address,
 }
 
+impl SLPTxType {
+    pub fn from_str(s: &str) -> Option<Self> {
+        match s {
+            "GENESIS" => Some(SLPTxType::Genesis),
+            "SEND"    => Some(SLPTxType::Send),
+            "MINT"    => Some(SLPTxType::Mint),
+            "COMMIT"  => Some(SLPTxType::Commit),
+            _         => None,
+        }
+    }
+
+    pub fn to_str(&self) -> &str {
+        match self {
+            SLPTxType::Genesis => "GENESIS",
+            SLPTxType::Send    => "SEND",
+            SLPTxType::Mint    => "MINT",
+            SLPTxType::Commit  => "COMMIT",
+        }
+    }
+}
+
+impl OutputType {
+    pub fn address(&self) -> Option<&cashcontracts::Address> {
+        match self {
+            OutputType::Address(addr) => Some(addr),
+            _ => None,
+        }
+    }
+
+    pub fn id(&self) -> i32 {
+        match self {
+            OutputType::Unknown => 0,
+            OutputType::OpReturn => 1,
+            OutputType::Address(addr) => match addr.addr_type() {
+                AddressType::P2SH => 2,
+                AddressType::P2PKH => 3,
+            }
+        }
+    }
+}
+
+impl TxType {
+    pub fn id(&self) -> i32 {
+        match self {
+            TxType::Default => 1,
+            TxType::SLP {..} => 2,
+        }
+    }
+}
+
 impl TxHistory {
     fn _process_address(address: &Option<String>) -> OutputType {
         match address {
@@ -89,11 +139,12 @@ impl TxHistory {
         }).unwrap_or(0)
     }
 
-    pub fn from_entries(entries: impl Iterator<Item=tx_result::TxEntry>,
-                        now: i64) -> Self {
-        let mut historic_txs = Vec::new();
-        //let mut trade_offers = Vec::new();
-        for (idx, entry) in entries.enumerate() {
+    pub fn from_entries(entries: &[tx_result::TxEntry],
+                        now: i64,
+                        config: &SLPDEXConfig) -> Self {
+        let mut historic_txs = Vec::with_capacity(entries.len());
+        let mut trade_offers = Vec::new();
+        for (idx, entry) in entries.iter().enumerate() {
             let inputs = entry.inputs.iter()
                 .map(|input| {
                     HistoricTxInput {
@@ -109,10 +160,10 @@ impl TxHistory {
                     HistoricTxOutput {
                         value_satoshis: output.e.v,
                         value_token_base: match &entry.slp {
-                            Some(slp) => slp.detail.outputs.get(i - 1).map(
+                            Some(slp) if i > 0 => slp.detail.outputs.get(i - 1).map(
                                 |output| Self::_parse_slp_amount(&output.amount, slp.detail.decimals)
                             ).unwrap_or(0),
-                            None => 0,
+                            _ => 0,
                         },
                         output: if output.b0 == (tx_result::StackItem::Op {op: 0x6a}) {
                             OutputType::OpReturn
@@ -122,16 +173,16 @@ impl TxHistory {
                     }
                 })
                 .collect::<Vec<_>>();
-            historic_txs.push(HistoricTx {
+            let historic_tx = HistoricTx {
                 hash: cashcontracts::tx_hex_to_hash(&entry.tx.h),
                 height: entry.blk.as_ref().map(|blk| blk.i),
                 timestamp: entry.blk.as_ref().map(|blk| blk.t as i64).unwrap_or(now),
-                tx_type: entry.slp
+                tx_type: entry.slp.as_ref()
                     .and_then(|slp| Some(TxType::SLP {
                         version: slp.detail.version_type,
-                        token_id: {
+                        token_hash: {
                             let mut token_id = [0; 32];
-                            token_id.copy_from_slice(&hex::decode(slp.detail.token_id).ok()?);
+                            token_id.copy_from_slice(&hex::decode(&slp.detail.token_id).ok()?);
                             token_id
                         },
                         slp_type: SLPTxType::from_str(&slp.detail.transaction_type)?,
@@ -139,30 +190,17 @@ impl TxHistory {
                     .unwrap_or(TxType::Default),
                 inputs,
                 outputs,
-            });
+            };
+            let trade_offer = TradeOffer::from_entry(&historic_tx, entry, config);
+            match trade_offer {
+                Some(trade_offer) => trade_offers.push((historic_txs.len(), trade_offer)),
+                None => {},
+            }
+            historic_txs.push(historic_tx);
         }
-        unimplemented!()
-        //HistoricTx {}
-    }
-}
-
-impl SLPTxType {
-    pub fn from_str(s: &str) -> Option<Self> {
-        match s {
-            "GENESIS" => Some(SLPTxType::Genesis),
-            "SEND"    => Some(SLPTxType::Send),
-            "MINT"    => Some(SLPTxType::Mint),
-            "COMMIT"  => Some(SLPTxType::Commit),
-            _         => None,
-        }
-    }
-}
-
-impl OutputType {
-    pub fn address(&self) -> Option<&cashcontracts::Address> {
-        match self {
-            OutputType::Address(addr) => Some(addr),
-            _ => None,
+        TxHistory {
+            txs: historic_txs,
+            trade_offers,
         }
     }
 }
@@ -194,7 +232,7 @@ impl TradeOffer {
         let power_bytes = base64::decode(power_b64).ok()?;
         let mut price_bytes = io::Cursor::new(base64::decode(price_b64).ok()?);
         let is_inverted = power_bytes.get(1) == Some(&1);
-        let script_price = price_bytes.read_u32::<LittleEndian>().ok()?;
+        let script_price = price_bytes.read_u32::<BigEndian>().ok()?;
         let factor = Self::_FACTORS[slp_decimals as usize];
         let factor_rational = rug::Rational::from((factor, 1));
         let approx_price_per_token = if is_inverted {
@@ -222,8 +260,8 @@ impl TradeOffer {
         entry.inputs.iter().enumerate().find_map(|(i, input)| {
             if input.b0 == tx_result::StackItem::Str(base64::encode("EXCH")) &&
                     input.b1 == (tx_result::StackItem::Op {op: 0x52}) {
-                let (token_id, token_type, slp_type) = match &tx.tx_type {
-                    TxType::SLP {token_id, version, slp_type} => (token_id, *version, slp_type),
+                let (token_hash, token_type, slp_type) = match &tx.tx_type {
+                    TxType::SLP {token_hash, version, slp_type} => (token_hash, *version, slp_type),
                     TxType::Default => return None,
                 };
                 let price = entry.slp.as_ref()
@@ -233,7 +271,7 @@ impl TradeOffer {
                         input.b3.get_str()?,
                     ))?;
                 let receiving_address = cashcontracts::Address::from_bytes(
-                    cashcontracts::AddressType::P2PKH,
+                    AddressType::P2PKH,
                     {
                         let mut payload = [0; 20];
                         payload.copy_from_slice(&base64::decode(input.b4.get_str()?).ok()?);
@@ -244,7 +282,7 @@ impl TradeOffer {
                 let contract_vals = tx.outputs.get(output_idx as usize)
                     .and_then(|output: &HistoricTxOutput| {
                         let address = output.output.address()?;
-                        if address.addr_type() == cashcontracts::AddressType::P2SH {
+                        if address.addr_type() == AddressType::P2SH {
                             let hash = cashcontracts::hash160(
                                 &cashcontracts::AdvancedTradeOffer {
                                     value: output.value_satoshis,
@@ -252,7 +290,7 @@ impl TradeOffer {
                                     version: 2,
                                     power: price.power,
                                     is_inverted: price.is_inverted,
-                                    token_id: token_id.clone(),
+                                    token_id: token_hash.clone(),
                                     token_type: token_type as u8,
                                     sell_amount_token: output.value_token_base,
                                     price: price.script_price,
