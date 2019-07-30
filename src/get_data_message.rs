41f0664f11131ddc3ae2e182fe7ab7719cb274e4
tobiasruck <ruck.tobias@gmail.com>
2019-07-30 23:17:37 +0200

    Added GetDataMessage

---
+++ src/get_data_message.rs
@@ -0,0 +1,26 @@
+use crate::message::Message;
+use crate::inv_message::{ObjectType, InvVector};
+use cashcontracts::serialize::{read_var_int, write_var_int};
+use byteorder::{LittleEndian, WriteBytesExt};
+use std::{io, io::Write};
+
+#[derive(Clone, Debug)]
+pub struct GetDataMessage {
+    pub inv_vectors: Vec<InvVector>,
+}
+
+impl GetDataMessage {
+    pub fn command() -> &'static [u8] {
+        b"getdata"
+    }
+
+    pub fn message(&self) -> Message {
+        let mut payload = Vec::new();
+        write_var_int(&mut payload, self.inv_vectors.len() as u64).unwrap();
+        for inv_vector in self.inv_vectors.iter() {
+            payload.write_u32::<LittleEndian>(inv_vector.type_id as u32).unwrap();
+            payload.write(&inv_vector.hash).unwrap();
+        }
+        Message::from_payload(Self::command(), payload)
+    }
+}
