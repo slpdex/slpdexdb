2e0668626cf50746ab8d15eadcdd5e7f3b833129
tobiasruck <ruck.tobias@gmail.com>
2019-08-04 15:54:27 +0200

    Added ResyncActor and TxActor to main.rs

--- slpdexdb_endpoint/src/main.rs
+++ slpdexdb_endpoint/src/main.rs
@@ -1,15 +1,30 @@
 mod actors;
+mod query;
+mod msg;
 
 use std::str::FromStr;
 use std::net;
 use tokio_tcp::TcpStream;
 use actix::prelude::*;
+use diesel::prelude::*;
 
 use slpdexdb_node::actors as node_actors;
+use slpdexdb_base::SLPDEXConfig;
+use slpdexdb_db::Db;
+use crate::actors::{TxActor, ResyncActor};
 
+
+fn connect_db() -> Db {
+    let connection_str = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let connection = PgConnection::establish(&connection_str).unwrap();
+    Db::new(connection)
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(actix::System::run(|| {
+        let resync_addr = SyncArbiter::start(2, || {
+            ResyncActor::new(connect_db(), SLPDEXConfig::default())
+        });
         let db_addr = actors::DbActor::create().unwrap();
         let db_addr = slpdexdb_node::DbActor::start(slpdexdb_node::DbActor {
             add_header_query: db_addr.clone().recipient(),
@@ -20,7 +35,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             TcpStream::connect(&addr)
                 .and_then(move |stream| {
                     println!("connected");
-                    let addr = node_actors::NodeActor::create_from_stream_db(stream, db_addr);
+                    let node_addr = node_actors::NodeActor::create_from_stream_db(stream, db_addr);
+                    let tx_addr = TxActor::start(TxActor::new(
+                        connect_db(),
+                        SLPDEXConfig::default(),
+                        node_addr,
+                        resync_addr,
+                    ));
                     futures::future::ok(())
                 })
                 .map_err(|err| {
