3b7622304549b5a286396f50afb69b4d1fd8553c
tobiasruck <ruck.tobias@gmail.com>
2019-08-06 15:05:32 +0200

    Added peers_actor and ws_actor

--- slpdexdb_endpoint/src/actors/mod.rs
+++ slpdexdb_endpoint/src/actors/mod.rs
@@ -1,7 +1,11 @@
 mod db_actor;
 mod resync_actor;
 mod tx_actor;
+mod peers_actor;
+mod ws_actor;
 
 pub use db_actor::*;
 pub use resync_actor::*;
 pub use tx_actor::*;
+pub use peers_actor::*;
+pub use ws_actor::*;
