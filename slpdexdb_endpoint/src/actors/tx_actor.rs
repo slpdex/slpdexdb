d0c2786147c8148509ae2110826906943edd7f5b
tobiasruck <ruck.tobias@gmail.com>
2019-08-06 15:05:07 +0200

    Added handlers for ActivateAddress, DeactivateAddress, FetchAddressUtxos and FetchAddressTxDeltas to TxActor

--- slpdexdb_endpoint/src/actors/tx_actor.rs
+++ slpdexdb_endpoint/src/actors/tx_actor.rs
@@ -1,27 +1,26 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use actix::prelude::*;
 use std::collections::HashSet;
+use std::convert::identity;
 use slpdexdb_base::{Error, SLPDEXConfig};
-use slpdexdb_db::{Db, TxSource, TxHistory, OutputType, UpdateSubjectType, UpdateHistory};
+use slpdexdb_db::{Db, TxSource, TxHistory, OutputType, UpdateSubjectType, UpdateHistory, Utxo, TxDelta};
 use slpdexdb_node::actors::{NodeActor, IncomingMsg};
 use slpdexdb_node::msg::Subscribe;
 use slpdexdb_node::messages::TxMessage;
-use crate::msg::{SetAddressActive, ResyncAddress};
+use crate::msg::{ActivateAddress, DeactivateAddress, ResyncAddress, FetchAddressUtxos, FetchAddressTxDeltas};
 use crate::actors::ResyncActor;
 
 pub struct TxActor {
     db: Db,
     config: SLPDEXConfig,
-    node: Addr<NodeActor>,
     resync: Addr<ResyncActor>,
 }
 
 impl TxActor {
     pub fn new(db: Db,
                config: SLPDEXConfig,
-               node: Addr<NodeActor>,
                resync: Addr<ResyncActor>) -> Self {
-        TxActor { db, node, config, resync }
+        TxActor { db, config, resync }
     }
 }
 
@@ -29,7 +28,7 @@ impl Actor for TxActor {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        self.node.do_send(Subscribe::Tx(ctx.address().recipient()));
+        //self.node.do_send(Subscribe::Tx(ctx.address().recipient()));
     }
 }
 
@@ -39,7 +38,7 @@ impl Handler<IncomingMsg<TxMessage>> for TxActor {
     fn handle(&mut self, msg: IncomingMsg<TxMessage>, ctx: &mut Self::Context) -> Self::Result {
         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as i64;
         let tx = msg.0.tx.clone();
-        let history = TxHistory::from_txs(&[tx], timestamp, &self.config, &self.db);
+        let mut history = TxHistory::from_txs(&[tx], timestamp, &self.config, &self.db);
         let addresses = history.txs.iter()
             .flat_map(|tx| {
                 tx.outputs.iter()
@@ -51,24 +50,53 @@ impl Handler<IncomingMsg<TxMessage>> for TxActor {
                     })
             })
             .collect::<Vec<_>>();
-        if !self.db.is_active_address(&addresses)?.iter().any(|x| *x) {
+        if history.trade_offers.len() == 0 &&
+                !self.db.is_active_address(&addresses)?.iter().any(|x| *x) {
             return Ok(())
         }
+        history.validate_slp(&TxSource::new(), &self.db, &self.config)?;
         self.db.add_tx_history(&history)?;
         Ok(())
     }
 }
 
-impl Handler<SetAddressActive> for TxActor {
+impl Handler<ActivateAddress> for TxActor {
+    type Result = Response<(), Error>;
+
+    fn handle(&mut self, msg: ActivateAddress, ctx: &mut Self::Context) -> Self::Result {
+        let ActivateAddress(address) = msg;
+        let resync = self.resync.clone();
+        Response::fut(
+            futures::future::result(self.db.set_address_active(&address, true)).from_err()
+                .and_then(move |_| resync.send(ResyncAddress(address)).from_err())
+                .and_then(identity)
+        )
+    }
+}
+
+impl Handler<DeactivateAddress> for TxActor {
     type Result = Result<(), Error>;
 
-    fn handle(&mut self, msg: SetAddressActive, ctx: &mut Self::Context) -> Self::Result {
-        let address = msg.0;
-        let is_active = msg.1;
-        self.db.set_address_active(&address, is_active)?;
-        if is_active {
-            self.resync.do_send(ResyncAddress(address))
-        }
-        Ok(())
+    fn handle(&mut self, msg: DeactivateAddress, ctx: &mut Self::Context) -> Self::Result {
+        let DeactivateAddress(address) = msg;
+        Ok(self.db.set_address_active(&address, false)?)
+    }
+}
+
+impl Handler<FetchAddressUtxos> for TxActor {
+    type Result = Result<Vec<Utxo>, Error>;
+
+    fn handle(&mut self, msg: FetchAddressUtxos, ctx: &mut Self::Context) -> Self::Result {
+        let FetchAddressUtxos(address) = msg;
+        Ok(self.db.utxos_address(&address)?)
+    }
+}
+
+impl Handler<FetchAddressTxDeltas> for TxActor {
+    type Result = Result<Vec<TxDelta>, Error>;
+
+    fn handle(&mut self, msg: FetchAddressTxDeltas, ctx: &mut Self::Context) -> Self::Result {
+        let FetchAddressTxDeltas(address) = msg;
+        Ok(self.db.address_tx_deltas(&address)?)
     }
 }
