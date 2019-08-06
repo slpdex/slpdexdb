736f4de7cc917bf797541c7ae95ffe9d31cf18df
tobiasruck <ruck.tobias@gmail.com>
2019-08-06 15:06:22 +0200

    Added FetchAddressTxDeltas, FetchAddressUtxos to msg, split SetAddressActive into (De)ActivateAddress

--- slpdexdb_endpoint/src/msg.rs
+++ slpdexdb_endpoint/src/msg.rs
@@ -1,10 +1,26 @@
 use actix::prelude::*;
 use cashcontracts::Address;
 use slpdexdb_base::Error;
+use std::net;
+use slpdexdb_db::{Utxo, TxDelta};
 
-pub struct SetAddressActive(pub Address, pub bool);
+pub struct ConnectToPeer {
+    pub socket_addr: net::SocketAddr,
+}
 
-impl Message for SetAddressActive {
+impl Message for ConnectToPeer {
+    type Result = Result<(), Error>;
+}
+
+pub struct ActivateAddress(pub Address);
+
+impl Message for ActivateAddress {
+    type Result = Result<(), Error>;
+}
+
+pub struct DeactivateAddress(pub Address);
+
+impl Message for DeactivateAddress {
     type Result = Result<(), Error>;
 }
 
@@ -14,3 +30,14 @@ impl Message for ResyncAddress {
     type Result = Result<(), Error>;
 }
 
+pub struct FetchAddressUtxos(pub Address);
+
+impl Message for FetchAddressUtxos {
+    type Result = Result<Vec<Utxo>, Error>;
+}
+
+pub struct FetchAddressTxDeltas(pub Address);
+
+impl Message for FetchAddressTxDeltas {
+    type Result = Result<Vec<TxDelta>, Error>;
+}
