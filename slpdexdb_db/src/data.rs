834f371c2c6e4fa24584d2a953f31b8d2c0c6daa
tobiasruck <ruck.tobias@gmail.com>
2019-08-10 16:07:05 +0200

    Added SpentUtxo, NewUtxo, TradeOfferFilter and tx_hash_from_slice, address_hash_from_slice to data.rs

--- slpdexdb_db/src/data.rs
+++ slpdexdb_db/src/data.rs
@@ -1,5 +1,7 @@
+use cashcontracts::Address;
 use slpdexdb_base::SLPAmount;
 
+#[derive(Clone, Debug)]
 pub struct Utxo {
     pub tx_hash: [u8; 32],
     pub vout: i32,
@@ -9,10 +11,47 @@ pub struct Utxo {
 }
 
 #[derive(Clone, Debug)]
+pub struct SpentUtxo {
+    pub tx_hash: [u8; 32],
+    pub vout: i32,
+}
+
+#[derive(Clone, Debug)]
+pub enum NewUtxo {
+    Address {
+        tx_hash: [u8; 32],
+        vout: i32,
+        address: Address,
+    },
+    TradeOffer {
+        tx_hash: [u8; 32],
+        vout: i32,
+    },
+}
+
+#[derive(Clone, Debug)]
+pub enum TradeOfferFilter {
+    TokenHash([u8; 32]),
+    ReceivingAddress(Address),
+}
+
+#[derive(Clone, Debug)]
 pub struct TxDelta {
     pub tx_hash: [u8; 32],
     pub delta_satoshis: i64,
     pub delta_token: SLPAmount,
     pub token_hash: Option<[u8; 32]>,
     pub timestamp: i64,
+}
+
+pub fn tx_hash_from_slice(slice: &[u8]) -> [u8; 32] {
+    let mut hash = [0; 32];
+    hash.copy_from_slice(&slice);
+    hash
+}
+
+pub fn address_hash_from_slice(slice: &[u8]) -> [u8; 20] {
+    let mut hash = [0; 20];
+    hash.copy_from_slice(&slice);
+    hash
 }
