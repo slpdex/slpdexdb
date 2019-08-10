7dfa547d4d62386631628d99a9fe8871435d5483
tobiasruck <ruck.tobias@gmail.com>
2019-08-11 01:37:21 +0200

    Changed ws path to /ws, reduces ResyncActor threads to 1

--- slpdexdb_endpoint/src/main.rs
+++ slpdexdb_endpoint/src/main.rs
@@ -4,15 +4,14 @@ mod msg;
 
 use std::str::FromStr;
 use std::net;
-use tokio_tcp::TcpStream;
+use std::sync::{Arc, Mutex};
 use actix::prelude::*;
 use diesel::prelude::*;
 
-use actix_web::{middleware, web, App, HttpResponse, HttpRequest, HttpServer, Responder};
+use actix_web::{middleware, web, App, HttpResponse, HttpRequest, HttpServer};
 use actix_web_actors::ws;
 
-use cashcontracts::{Address, tx_hash_to_hex};
-use slpdexdb_node::actors as node_actors;
+use cashcontracts::{Address};
 use slpdexdb_base::SLPDEXConfig;
 use slpdexdb_db::Db;
 use crate::actors::{TxActor, ResyncActor, PeersActor, WsActor};
@@ -40,7 +39,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     let port = std::env::var("PORT").unwrap_or("7501".to_string());
     actix::System::run(move || {
-        let resync_addr = SyncArbiter::start(2, || {
+        let resync_addr = SyncArbiter::start(1, || {
             ResyncActor::new(connect_db(), SLPDEXConfig::default())
         });
         let db_addr = actors::DbActor::create().unwrap();
@@ -48,17 +47,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             add_header_query: db_addr.clone().recipient(),
             header_tip_query: db_addr.recipient(),
         });
-        let tx_addr = TxActor::start(TxActor::new(connect_db(), SLPDEXConfig::default(), resync_addr));
+        let tx_addr = TxActor::start_with(Arc::new(Mutex::new(connect_db())), SLPDEXConfig::default(), resync_addr);
         let peers_addr = PeersActor::start(PeersActor::new(tx_addr.clone(), db_addr));
         let socket_addr = net::SocketAddr::from_str("100.1.209.114:8333").unwrap();
-        peers_addr.do_send(ConnectToPeer { socket_addr });
+
+        Arbiter::spawn(
+            peers_addr.send(ConnectToPeer { socket_addr })
+                .from_err()
+                .and_then(|x| x)
+                .map_err(|err| {eprintln!("{}", err);})
+        );
 
         HttpServer::new(move || {
             App::new()
                 .wrap(middleware::Logger::default())
                 .data(tx_addr.clone())
                 .service(
-                    web::resource("/address/{address}").route(web::get().to(index))
+                    web::resource("/ws/{address}").route(web::get().to(index))
                 )
         })
             .bind(format!("127.0.0.1:{}", port)).unwrap()
