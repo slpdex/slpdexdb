e5d716edc73bc0e5ff469cdd6262c583637db788
tobiasruck <ruck.tobias@gmail.com>
2019-08-11 01:36:29 +0200

    Added FetchTradeOfferUtxos, SubscribeToEvent, UnsubscribeFromEvent, TxEvent, TxBroadcastEvent, NewTransactions, ProcessTransactions messages

--- slpdexdb_endpoint/src/msg.rs
+++ slpdexdb_endpoint/src/msg.rs
@@ -2,7 +2,12 @@ use actix::prelude::*;
 use cashcontracts::Address;
 use slpdexdb_base::Error;
 use std::net;
-use slpdexdb_db::{Utxo, TxDelta};
+use slpdexdb_base::SLPDEXConfig;
+use slpdexdb_db::{Db, Utxo, SpentUtxo, TxDelta, TradeOfferFilter, TradeOffer, TxHistory};
+use std::collections::{HashSet, HashMap};
+use std::sync::{Arc, Mutex};
+use crate::actors::TxSubscribers;
+
 
 pub struct ConnectToPeer {
     pub socket_addr: net::SocketAddr,
@@ -30,6 +35,12 @@ impl Message for ResyncAddress {
     type Result = Result<(), Error>;
 }
 
+pub struct FetchTradeOfferUtxos(pub TradeOfferFilter);
+
+impl Message for FetchTradeOfferUtxos {
+    type Result = Result<Vec<TradeOffer>, Error>;
+}
+
 pub struct FetchAddressUtxos(pub Address);
 
 impl Message for FetchAddressUtxos {
@@ -40,4 +51,89 @@ pub struct FetchAddressTxDeltas(pub Address);
 
 impl Message for FetchAddressTxDeltas {
     type Result = Result<Vec<TxDelta>, Error>;
+}
+
+pub enum SubscribeToEvent {
+    Address(Address, Recipient<TxEvent>),
+    Tokens(Vec<[u8; 32]>, Recipient<TxEvent>),
+}
+
+impl Message for SubscribeToEvent {
+    type Result = ();
+}
+
+pub enum UnsubscribeFromEvent {
+    Address(Address, Recipient<TxEvent>),
+}
+
+impl Message for UnsubscribeFromEvent {
+    type Result = ();
+}
+
+#[derive(Clone)]
+pub enum TxEvent {
+    AddressUtxoDelta {
+        add_utxos: Arc<Vec<Utxo>>,
+        remove_utxos: Arc<Vec<SpentUtxo>>,
+    },
+    TradeOfferUtxoDelta {
+        token_hash: [u8; 32],
+        add_utxos: Arc<Vec<TradeOffer>>,
+        remove_utxos: Arc<Vec<SpentUtxo>>,
+    },
+    AddressNewTxDeltas {
+        tx_deltas: Arc<Vec<TxDelta>>,
+    },
+}
+
+impl Message for TxEvent {
+    type Result = Result<(), Error>;
+}
+
+type SyncTxSubscribers = Arc<Mutex<TxSubscribers>>;
+
+pub enum TxBroadcastEvent {
+    AddressUtxoDelta {
+        add_utxos: HashMap<Address, Vec<Utxo>>,
+        remove_utxos: HashMap<Address, Vec<SpentUtxo>>,
+        subscribers: SyncTxSubscribers,
+    },
+    TradeOfferUtxoDelta {
+        add_utxos: HashMap<[u8; 32], Vec<TradeOffer>>,
+        remove_utxos: HashMap<[u8; 32], Vec<SpentUtxo>>,
+        subscribers: SyncTxSubscribers,
+    },
+    AddressNewTxDeltas {
+        tx_deltas: HashMap<Address, Vec<TxDelta>>,
+        subscribers: SyncTxSubscribers,
+    },
+}
+
+impl Message for TxBroadcastEvent {
+    type Result = Result<(), Error>;
+}
+
+#[derive(Clone)]
+pub struct NewTransactions {
+    pub now: i64,
+    pub db: Arc<Mutex<Db>>,
+    pub tx_history: Arc<TxHistory>,
+    pub relevant_addresses: Arc<HashSet<Address>>,
+    pub subscribers: SyncTxSubscribers,
+}
+
+impl Message for NewTransactions {
+    type Result = Result<(), Error>;
+}
+
+pub struct ProcessTransactions {
+    pub txs: Vec<cashcontracts::Tx>,
+    pub db: Arc<Mutex<Db>>,
+    pub config: SLPDEXConfig,
+    pub subscribers: Arc<Mutex<TxSubscribers>>,
+    pub broadcasts: Vec<Recipient<NewTransactions>>,
+}
+
+impl Message for ProcessTransactions {
+    type Result = Result<(), Error>;
 }
