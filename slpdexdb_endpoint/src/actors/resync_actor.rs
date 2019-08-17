34eab57ff40460834ae6c54f20c0808e2bee1a1e
tobiasruck <ruck.tobias@gmail.com>
2019-08-17 15:37:56 +0200

    Fix resync_actor to distinguish between confirmed and unconfirmed addresses

--- slpdexdb_endpoint/src/actors/resync_actor.rs
+++ slpdexdb_endpoint/src/actors/resync_actor.rs
@@ -4,13 +4,14 @@ use std::collections::HashSet;
 use actix::prelude::*;
 use cashcontracts::Address;
 use slpdexdb_base::{Error, SLPDEXConfig};
-use slpdexdb_db::{Db, TxSource, TokenSource, UpdateSubjectType, UpdateHistory, TxHistory, Token, OutputType};
+use slpdexdb_db::{Db, TxSource, TokenSource, UpdateSubject, UpdateSubjectType, UpdateHistory, TxHistory, Token, OutputType, Confirmedness};
 use crate::msg::{ResyncAddress, ProcessTransactions, NewTransactions};
 
 
 fn _resync(db: &Db, config: &SLPDEXConfig) -> Result<(), Error> {
     _resync_tokens(db)?;
-    _resync_trade_offers(db, config)?;
+    _resync_trade_offers(db, config, true)?;
+    _resync_trade_offers(db, config, false)?;
     Ok(())
 }
 
@@ -18,9 +19,13 @@ fn _resync_tokens(db: &Db) -> Result<(), Error> {
     let token_source = TokenSource::new();
     loop {
         let current_height = db.header_tip()?.map(|(_, height)| height).unwrap_or(0);
-        let last_update =
-            db.last_update(UpdateSubjectType::Token, None)?
-                .unwrap_or(UpdateHistory::initial(UpdateSubjectType::Token, None));
+        let subject = UpdateSubject {
+            subject_type: UpdateSubjectType::Token,
+            hash: None,
+            is_confirmed: true,
+        };
+        let last_update = db.last_update(subject.clone())?
+                .unwrap_or(UpdateHistory::initial(subject));
         let token_entries = token_source.request_tokens(&last_update.next_filters())?;
         let tokens = token_entries.into_iter()
             .filter_map(|token_entry| {
@@ -40,41 +45,50 @@ fn _resync_tokens(db: &Db) -> Result<(), Error> {
     Ok(())
 }
 
-fn _resync_trade_offers(db: &Db, config: &SLPDEXConfig) -> Result<(), Error> {
+fn _resync_trade_offers(db: &Db, config: &SLPDEXConfig, is_confirmed: bool) -> Result<(), Error> {
     let tx_source = TxSource::new();
     loop {
         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
         let current_height = db.header_tip()?.map(|(_, height)| height).unwrap_or(0);
-        let last_update =
-            db.last_update(UpdateSubjectType::Exch, None)?
-                .unwrap_or(UpdateHistory::initial(UpdateSubjectType::Exch, None));
-        let tx_entries = tx_source.request_txs(&last_update.next_filters(), config)?;
+        let confirmedness = if is_confirmed { Confirmedness::Confirmed }
+                            else { Confirmedness::Unconfirmed };
+        let subject = UpdateSubject {
+            subject_type: UpdateSubjectType::Exch,
+            hash: None,
+            is_confirmed,
+        };
+        let last_update = db.last_update(subject.clone())?
+                .unwrap_or_else(|| UpdateHistory::initial(subject.clone()));
+        let tx_entries = tx_source.request_txs(&last_update.next_filters(), config, confirmedness)?;
         let history = TxHistory::from_entries(&tx_entries, timestamp as i64, config);
         if history.txs.len() == 0 {
             break
         }
         db.add_tx_history(&history)?;
         db.add_update_history(
-            &UpdateHistory::from_tx_history(&history, UpdateSubjectType::Exch, None, current_height)
+            &UpdateHistory::from_tx_history(&history, subject, current_height)
         )?;
     }
     db.update_utxo_set_exch()?;
     Ok(())
 }
 
-fn _resync_address(db: &Db, config: &SLPDEXConfig, address: &Address) -> Result<(), Error> {
+fn _resync_address(db: &Db, config: &SLPDEXConfig, address: &Address, is_confirmed: bool) -> Result<(), Error> {
     loop {
         let tx_source = TxSource::new();
         let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
         let current_height = db.header_tip()?.map(|(_, height)| height).unwrap_or(0);
-        let last_update =
-            db.last_update(UpdateSubjectType::AddressHistory, Some(address.bytes().to_vec()))?
-                .unwrap_or(UpdateHistory::initial(
-                    UpdateSubjectType::AddressHistory,
-                    Some(address.bytes().to_vec()),
-                ));
+        let confirmedness = if is_confirmed { Confirmedness::Confirmed }
+                            else { Confirmedness::Unconfirmed };
+        let subject = UpdateSubject {
+            subject_type: UpdateSubjectType::AddressHistory,
+            hash: Some(address.bytes().to_vec()),
+            is_confirmed,
+        };
+        let last_update = db.last_update(subject.clone())?
+            .unwrap_or(UpdateHistory::initial(subject.clone()));
         println!("last update: {}", last_update);
-        let tx_entries = tx_source.request_txs(&last_update.next_filters(), config)?;
+        let tx_entries = tx_source.request_txs(&last_update.next_filters(), config, confirmedness)?;
         let history = TxHistory::from_entries(&tx_entries, timestamp as i64, config);
         if history.txs.len() > 0 {
             db.add_tx_history(&history)?;
@@ -82,8 +96,7 @@ fn _resync_address(db: &Db, config: &SLPDEXConfig, address: &Address) -> Result<
         db.add_update_history(
             &UpdateHistory::from_tx_history(
                 &history,
-                UpdateSubjectType::AddressHistory,
-                Some(address.bytes().to_vec()),
+                subject,
                 current_height,
             )
         )?;
@@ -121,7 +134,9 @@ impl Handler<ResyncAddress> for ResyncActor {
 
     fn handle(&mut self, msg: ResyncAddress, _ctx: &mut Self::Context) -> Self::Result {
         let address = msg.0;
-        _resync_address(&self.db, &self.config, &address)
+        _resync_address(&self.db, &self.config, &address, true)?;
+        _resync_address(&self.db, &self.config, &address, false)?;
+        Ok(())
     }
 }
 
